650d1fe30e918dfecc3811835ae06fe7b8110cd2
smanimehalai <manimehalai.s@adlinktech.com>
2026-02-23 12:34:39 +0000

    Test commit 2 to check edk2 docker build trigger

--- make_ComHpcAlt.sh
+++ make_ComHpcAlt.sh
@@ -11,7 +11,7 @@ DEVEL_MODE=0
 # Firmware Version
 #
   VER=2.10
-  BUILD=100.02_Docker_Test
+  BUILD=100.02_Docker_Test1
 # DEBUG=0
 
 . adlink-platforms/Platform/Ampere/ComHpcAltPkg/edk2_adlink-ampere-altra/tools/make_adlink.sh ComHpcAlt A2
