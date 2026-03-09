0947d19c4f3462e9d08b78ad0f4ca9e607874cfb
smanimehalai <manimehalai.s@adlinktech.com>
2026-03-09 07:46:24 +0000

    Git build trogger test 4

--- make_ComHpcAlt.sh
+++ make_ComHpcAlt.sh
@@ -11,7 +11,7 @@ DEVEL_MODE=0
 # Firmware Version
 #
   VER=2.10
-  BUILD=100.02_Docker_Trigerr3
+  BUILD=100.02_Docker_Trigerr4
 # DEBUG=0
 
 . adlink-platforms/Platform/Ampere/ComHpcAltPkg/edk2_adlink-ampere-altra/tools/make_adlink.sh ComHpcAlt A2
